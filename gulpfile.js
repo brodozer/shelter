0ae0a18decbef5be2000c78fd6a6f6077f4b7681
Oleksandr Perepelitsa <perepelitsa.alex@gmail.com>
2022-11-17 12:38:40 +0100

    feat: add babelrc, remove console in production

--- gulpfile.js
+++ gulpfile.js
@@ -151,16 +151,6 @@ const js = () => {
 							exclude: /node_modules/,
 							use: {
 								loader: "babel-loader",
-								options: {
-									presets: [
-										[
-											"@babel/preset-env",
-											{
-												targets: "defaults",
-											},
-										],
-									],
-								},
 							},
 						},
 					],
